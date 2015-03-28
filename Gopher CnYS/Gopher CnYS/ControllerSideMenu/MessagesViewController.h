d1f7361faeb9d75fd052afc7d57b40dc7be63389
Vu Tiet <vutiet@gmail.com>
2015-03-28 15:18:35 +0700

    add

--- Gopher CnYS/Gopher CnYS/ControllerSideMenu/MessagesViewController.h
+++ Gopher CnYS/Gopher CnYS/ControllerSideMenu/MessagesViewController.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 #import "BaseViewController.h"
 
-@interface MessagesViewController : BaseViewController
+@interface MessagesViewController : BaseViewController <UITableViewDataSource, UITableViewDelegate>
 
 - (IBAction)pushViewController:(id)sender;
 
--- Gopher CnYS/Gopher CnYS/ControllerSideMenu/MessagesViewController.m
+++ Gopher CnYS/Gopher CnYS/ControllerSideMenu/MessagesViewController.m
@@ -10,9 +10,17 @@
 
 @interface MessagesViewController ()
 
+@property (nonatomic, weak) IBOutlet UITableView *messagesListTable;
+
 @end
 
 @implementation MessagesViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.messagesListTable.allowsMultipleSelectionDuringEditing = NO;
+    [self.messagesListTable registerClass:[UITableViewCell class] forCellReuseIdentifier:@"MessageListCell"];
+}
 
 -(void)viewWillAppear:(BOOL)animated
 {
@@ -28,6 +36,34 @@
     viewController.title = @"Pushed Controller";
     viewController.view.backgroundColor = [UIColor whiteColor];
     [self.navigationController pushViewController:viewController animated:YES];
+}
+
+#pragma mark - UITableView Datasource & Delegate
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 10;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+
+    static NSString *cellIdentifier = @"MessageListCell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier forIndexPath:indexPath];
+    
+    cell.textLabel.text = [NSString stringWithFormat:@"row %ld", (long)indexPath.row];
+    
+    return cell;
+
+}
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        NSLog(@"aww, delete hit");
+    }
 }
 
 @end
